32e69215271c3de6780da49943004c517d388544
mahad1506 <mahadatif5%gmail.com>
2026-08-19 16:50:03 +0500

    "Initial Commit (2)"

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,6 @@
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: unknown[]) {
+export function cn(...inputs) {
   const classes = inputs.flat(Infinity).filter(Boolean).join(" ");
   return twMerge(classes);
 }
